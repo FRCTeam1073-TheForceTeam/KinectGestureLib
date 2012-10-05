1c7e8809f2abbf53293672d40547bf6a8148b0a0
Evin Ugur <evinoog96@gmail.com>
2012-10-05 12:20:59 -0400

    Added Gestures
    These simple classes extend off of the abstract Gesture class and are
    easy to write because you don't have to deal with nui data. It's all
    static from the parent class, so you just have to focus on writing the
    gesture, and setting a time delay, if any.

---
+++ KinectGestureBaseLib/KinectGestureBaseLib/Gestures/ArmsExtended.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KinectGestureBase;
+namespace KinectGestureBase.Gestures
+{
+    class ArmsExtended : Gesture
+    {
+        public ArmsExtended() { }
+        protected override double getDelayTimeSeconds()
+        {
+            return 0.75;
+        }
+        protected override bool isDoingGesture()
+        {
+            float x_threshold = 0.6f;
+            float y_threshold = 0.1f;
+            bool x = getDistanceX(HAND_LEFT, SHOULDER_LEFT) >= x_threshold && getDistanceX(HAND_RIGHT, SHOULDER_RIGHT) >+ x_threshold;
+            bool y = getDistanceY(HAND_LEFT, HAND_RIGHT) <= y_threshold;    //make sure they're in a paralell y plane
+            return x && y;
+        }
+        public override string getDescription()
+        {
+            return "Arms extended";
+        }
+    }
+}
---
+++ KinectGestureBaseLib/KinectGestureBaseLib/Gestures/SpreadLegs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KinectGestureBase.Gestures
+{
+    class SpreadLegs : Gesture
+    {
+        private const float DEFAULT_DISTANCE = 0.55f;
+        private float spreadDistance;
+        public SpreadLegs() { spreadDistance = DEFAULT_DISTANCE; }
+        public SpreadLegs(float spreadDistance) { this.spreadDistance = spreadDistance; }
+        protected override double getDelayTimeSeconds() { return 1.0; }
+        public override string getDescription()
+        {
+            return "Gesture that sees if legs are spread by " + spreadDistance + " meters";
+        }
+        protected override bool isDoingGesture()
+        {
+            return getDistanceX(KNEE_LEFT, KNEE_RIGHT) >= spreadDistance;
+        }
+       
+    }
+}
