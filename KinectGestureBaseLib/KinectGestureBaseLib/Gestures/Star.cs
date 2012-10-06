349d162c60e2475d644100bca0026d1473d32cdf
Evin Ugur <evinoog96@gmail.com>
2012-10-05 20:36:45 -0400

    Added Gesture that Uses Other Gestures
    Demonstration of proper bypassing of the DateTime logic... since Vectors
    are static, construct a new Gesture on the fly when you're writing a
    Gesture that uses other Gestures and simply test the boolean check.

---
+++ KinectGestureBaseLib/KinectGestureBaseLib/Gestures/Star.cs
@@ -0,0 +1,25 @@
+﻿/*extension off of Gesture.cs to see if people are in a star shape. This class utilies some of the other gestures by bypassing 
+ *the datetime restriction.
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KinectGestureBase.Gestures
+{
+    class Star : Gesture
+    {
+        public Star() { }
+        protected override double getDelayTimeSeconds() { return 2.0; }
+        public override bool isDoingGesture()
+        {
+            if (getDistanceZ(HAND_LEFT, HAND_RIGHT) > 0.15f) return false;  //hands should be paralell in z plane.
+            if (!new SpreadLegs().isDoingGesture()) return false;
+            if (!new ArmsExtended().isDoingGesture()) return false;
+            return true;
+        }
+        public override string getDescription() { return "Star shaped function that demonstrates utilization of other gestures"; }
+    }
+}
