4343a12596fa66ecf1ee57d9488ddfe19a954b5f
Evin Ugur <evinoog96@gmail.com>
2012-10-09 07:33:37 -0400

    Added Example Windows Form
    Form Interfaces with the Kinect API and Gesture.cs by updating statics
    and dynamically checking for gestres.

---
+++ KinectGestureBaseLib/KinectGestureBaseLib/Gestures/HandForward.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace KinectGestureBase.Gestures
+{
+    class HandForward : Gesture
+    {
+        bool leftHand;
+        public HandForward(bool leftHand) { this.leftHand = leftHand; }
+        protected override double getDelayTimeSeconds() { return 1.5; }
+        public override bool isDoingGesture()
+        {
+            const float Z_THRESHOLD = 0.65f;
+            return getDistanceZ(leftHand ? HAND_LEFT : HAND_RIGHT, SHOULDER_CENTER) >= Z_THRESHOLD;
+        }
+        public override string getDescription()
+        {
+            return "Sees if the " + (leftHand ? "left" : "right") + " hand is extended towards the Kienct";
+        }
+    }
+}
